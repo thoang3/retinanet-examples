502dfdd28520b6ec02854b24d8740cb4c36a6ce6
htt1084 <hoangthanhtungvn@gmail.com>
2019-10-22 15:43:34 -0700

    updated note.txt

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,10 @@
-docker run -it --rm --runtime=nvidia \
+nvidia-docker run -it --rm \
     --shm-size 16G \
     --name RTTN-PT \
     -u 0 \
     -v /home/tung/playground:/home/tung/playground \
+    -v /media/tung/Data/data:/media/tung/Data/data \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -p 9096:6016 -p 5016:5016 \
-    -e NVIDIA_VISIBLE_DEVICES=1,5 -e CUDA_DEVICE_ORDER=PCI_BUS_ID \
-    rtnn/nvcr.io/nvidia/pytorch:19.05-py3 
+    -e NVIDIA_VISIBLE_DEVICES=0 -e CUDA_DEVICE_ORDER=PCI_BUS_ID \
+    rtnn/nvcr.io/nvidia/pytorch:19.09-py3 
--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-retinanet train retinanet_rn50fpn.pth --backbone ResNet50FPN \
-    --images /home/tung/playground/data/coco/images/train2017/ --annotations /home/tung/playground/data/coco/annotations/instances_train2017.json \
-    --val-images /home/tung/playground/coco/images/val2017/ --val-annotations /home/tung/playground/data/coco/annotations/instances_val2017.json
+retinanet train retinanet_rn50fpn.pth --backbone ResNet50FPN --lr 0.001 \
+    --images /media/tung/Data/data/coco/images/train2017/ --annotations /media/tung/Data/data/coco/annotations/instances_train2017.json \
+    --val-images /media/tung/Data/data/coco/images/val2017/ --val-annotations /media/tung/Data/data/coco/annotations/instances_val2017.json
