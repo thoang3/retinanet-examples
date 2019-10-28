9af180397da2f0257269a4f6cc4556b26ac509cb
Tung Hoang <tung@ras-aries.weworkers.io>
2019-10-28 10:02:44 -0700

    updated note

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,9 @@
 nvidia-docker run -it --rm \
     --shm-size 16G \
-    --name RTTN-PT05 \
+    --name RTTN-COPY-PT05 \
     -u 0 \
     -v /home/tung/playground:/home/tung/playground \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -p 9098:6018 -p 5018:5018 \
-    -e NVIDIA_VISIBLE_DEVICES=4 -e CUDA_DEVICE_ORDER=PCI_BUS_ID \
-    rtnn/nvcr.io/nvidia/pytorch:19.05-py3 
+    -p 9099:6019 -p 5019:5019 \
+    -e NVIDIA_VISIBLE_DEVICES=0,1,5,7 -e CUDA_DEVICE_ORDER=PCI_BUS_ID \
+    rtnn_copy/nvcr.io/nvidia/pytorch:19.05-py3 
--- train34.sh
+++ train34.sh
@@ -1,3 +1,3 @@
-retinanet train retinanet_rn34fpn.pth --backbone ResNet34FPN --lr 0.001 --batch 3 --iter 360000 \
-    --images /media/tung/Data/data/coco/images/train2017/ --annotations /media/tung/Data/data/coco/annotations/instances_train2017.json \
-    --val-images /media/tung/Data/data/coco/images/val2017/ --val-annotations /media/tung/Data/data/coco/annotations/instances_val2017.json
+retinanet train retinanet_rn34fpn.pth --backbone ResNet34FPN --lr 0.01 --iter 240000 --logdir log_resnet34fpn \
+    --images /home/tung/playground/data/coco/images/train2017/ --annotations /home/tung/playground/data/coco/annotations/instances_train2017.json \
+    --val-images /home/tung/playground/data/coco/images/val2017/ --val-annotations /home/tung/playground/data/coco/annotations/instances_val2017.json
