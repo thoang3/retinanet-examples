c09c5cf3246e5406bdd81330881e24395025cedb
thoang3 <hoangthanhtungvn@gmail.com>
2019-10-31 19:19:41 +0000

    update training 34 note

--- train34.sh
+++ train34.sh
@@ -1,3 +1,4 @@
-retinanet train retinanet_rn34fpn.pth --backbone ResNet34FPN --lr 0.01 --iter 240000 --logdir log_resnet34fpn \
+python retinanet/main.py train retinanet_rn34fpn.pth --backbone ResNet34FPN --lr 0.005 --batch 2 --warmup 8000 --iter 640000  --logdir log_resnet34fpn --milestones 320000 400000 \
     --images /home/tung/playground/data/coco/images/train2017/ --annotations /home/tung/playground/data/coco/annotations/instances_train2017.json \
     --val-images /home/tung/playground/data/coco/images/val2017/ --val-annotations /home/tung/playground/data/coco/annotations/instances_val2017.json
+
